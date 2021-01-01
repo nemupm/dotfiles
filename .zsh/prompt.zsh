4ca5290765fe3710675ce5cc5c373411770cfe6f
nemupm <nemupm@gmail.com>
2021-01-02 03:44:38 +0900

    use vi keymap for zle

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -4,7 +4,7 @@ zinit snippet PZT::modules/prompt
 # theme
 setopt promptsubst
 zinit snippet OMZT::gnzh
-
+# git repo info
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' formats '(%b)'
 zstyle ':vcs_info:*' actionformats '(%b|%a)'
@@ -14,5 +14,25 @@ precmd () {
  [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
 }
 
-PROMPT='%F{magenta}[%n@%m]%f %~
+PROMPT=''
+
+# show current mode
+function zle-keymap-select zle-line-init zle-line-finish
+{
+    case $KEYMAP in
+        main|viins)
+            PROMPT_2="%F{cyan}(insert)%f"
+            ;;
+        vicmd)
+            PROMPT_2="%F{white}(normal)%f"
+            ;;
+    esac
+
+    PROMPT='%F{magenta}[%n@%m]%f $PROMPT_2 %~
 %1(v|%F{green}%1v%f|)$ '
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
