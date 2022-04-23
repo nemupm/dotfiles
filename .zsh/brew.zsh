79d91480cd849e28000fd6649f09c256f1b80d35
nemupm <nemupm@gmail.com>
2022-04-23 20:31:28 +0900

    fix bug

--- .zsh/brew.zsh
+++ .zsh/brew.zsh
@@ -1,6 +1,6 @@
 if [ "$(uname -m)" = "arm64" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
+  export PATH=$(echo "$PATH" | sed -e 's/\/usr\/local\/bin://')
+  export PATH=$PATH:$HOME/.bin:/usr/local/bin
 fi
 
-export PATH=$(echo "$PATH" | sed -e 's/\/usr\/local\/bin://')
-export PATH=$PATH:$HOME/.bin:/usr/local/bin
